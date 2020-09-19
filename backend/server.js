9d496bbdc4557895b6b89fe258101d56dbf3a26e
User <user@localhost.localdomain>
2020-09-19 03:37:18 +0100

    added three pages split with router

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
+app.use('/trades', require(path.join(__dirname, 'routes/trades')))
+
+app.use('/leaderboard', require(path.join(__dirname, 'routes/leaderboard')));
+
+app.use('/', require(path.join(__dirname, 'routes/main')));
+
+app.use(function (req, res) {
+  res.status(404).render('404');
 });
 
-app.listen(port, () => {
-	console.log(`Server API listening at http://localhost:${port}`)
-});
+
+var server = app.listen(PORT, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("SavvyTrader listening at http://%s:%s", host, port);
+})
