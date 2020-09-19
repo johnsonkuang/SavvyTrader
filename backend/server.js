47e26c4197f35af1d7b2719b0fde3c323e05adb7
Vishal Devireddy <TwoTau@users.noreply.github.com>
2020-09-18 21:09:58 -0700

    Fix typo in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+	res.send('Hello World')
+});
 
-
-app.get('/', (req, res) -> {
-  res.send('Hello World')
-})
-
-
-var server = app.listen(PORT, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("SavvyTrader listening at http://%s:%s", host, port);
-})
+const server = app.listen(PORT, () => {
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log(`SavvyTrader listening at http://${host}:${port}`);
+});
