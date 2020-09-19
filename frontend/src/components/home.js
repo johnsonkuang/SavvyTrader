92b3b7fbc533a52a89e4c0b9ec3a108f14107ad1
Allen Su <allensu2001@gmail.com>
2020-09-19 15:23:02 -0700

    added shitty template ui before integration with backend

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -3,7 +3,9 @@ import Container from 'react-bootstrap/esm/Container';
 import StockGraph from './StockGraph';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
 
 class Home extends Component {
   render(){
@@ -15,7 +17,7 @@ class Home extends Component {
       <Container>
         <Row>
           <Col md={8}>
-            <h1>Home</h1>
+            <h1>Search Bar for Stonks Here?</h1>
             <div>
               <StockGraph></StockGraph>
             </div>
@@ -31,17 +33,54 @@ class Home extends Component {
             </Row>
             <Row>
               <Col>
-                <h2>User Prediction</h2>
                 <div>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  Compete against other traders in predicting the stock end price at the next hour, day, or week!
                 </div>
+                <Form>
+                  <Form.Group controlId="formGuessStockPrice">
+                    <Form.Label>Stock Price Prediction</Form.Label>
+                    <Form.Control type="Stock Price" placeholder="Enter Stock Price" />
+                  </Form.Group>
+                  <Form.Group controlId="formEndTime">
+                    <Form.Label>End Time</Form.Label>
+                    <Form.Control as="select">
+                      <option>Next Hour</option>
+                      <option>Next Day</option>
+                      <option>Next Week</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
               </Col>
             </Row>
             <Row>
               <Col>
                 <h2>All User Predictions</h2>
                 <div>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                      <th>End Time</th>
+                      <th>Average Predicted Stock Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                      <td>Next Hour</td>
+                      <td>$56.24</td>
+                    </tr>
+                    <tr>
+                      <td>Next Day</td>
+                      <td>57.98</td>
+                    </tr>
+                    <tr>
+                      <td>Next Week</td>
+                      <td>$61.54</td>
+                    </tr>
+                    </tbody>
+                  </Table>
                 </div>
               </Col>
             </Row>
