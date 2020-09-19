efb476af69a20084e759bf5606363d366733bd7d
Armand Mousavi <amousavi@cs.washington.edu>
2020-09-18 23:47:36 -0700

    added addPrediction function
    POST request to '/addPrediction' using json object in body of POST request. see comments for parameters. I tested it, you can try to tell me what to tweak y'all.

--- backend/FirestoreQueries.js
+++ backend/FirestoreQueries.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
 const admin = require('firebase-admin');
 // initialize firebase
 const serviceAccount = require('./savvytrader-30ec3-firebase-adminsdk.json');
@@ -8,12 +8,42 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-const traders = db.collection('traders');
-const trades = db.collection('trades');
 
-app.use(express.json());
+router.use(express.json());
 
-app.post('/addPrediction', (req, res) => {
-  const result = await trades.add(req.body);
-  console.log('Added document with ID: ', result.id);
+// add a new 'trade' prediction to the trades collection
+/*
+Add all fields for a trade/prediction except dateMade
+currentAmount:106.84
+dateResult:September 18, 2020 at 6:22:00 PM UTC-7
+intervalType:"hour"
+predictedAmount:108
+stockSymbol:"AAPL"
+trader:"vishal"
+*/
+router.post('/addPrediction', (req, res) => {
+  req.body.dateMade = admin.firestore.Timestamp.fromDate(new Date());
+  addToCollection(req.body, db, "trades").then(result => {
+    console.log('Added document with ID: ', result.id);
+    res.sendStatus(200);
+  })
+  .catch(() => {
+    console.log(result);
+    console.log('Error adding document/prediction');
+    res.sendStatus(500);
+  });
+});
+
+// add a new 'trader' / user to the traders collection
+/*
+set field for initial energy and nothing else for nauw UwU
+*/
+
+module.exports = router;
+
+
+async function addToCollection(data, db, col) {
+  const queryRef = db.collection(col);
+  const result = await queryRef.add(data);
+  return result;
 }
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
 	res.send('Hello World')
 });
+
+app.use('/', require('./FirestoreQueries.js'));
 
 const server = app.listen(PORT, () => {
 	const host = server.address().address;
