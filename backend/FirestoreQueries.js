01fd91e5d584a6a929c7d8bffa616a2a8cbf926b
Armand Mousavi <amousavi@cs.washington.edu>
2020-09-18 21:42:37 -0700

    simple addPrediction
    still have to add dateMade

--- backend/FirestoreQueries.js
+++ backend/FirestoreQueries.js
@@ -1,7 +1,8 @@
+const express = require('express');
+const app = express();
 const admin = require('firebase-admin');
-
+// initialize firebase
 const serviceAccount = require('./savvytrader-30ec3-firebase-adminsdk.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
@@ -9,3 +10,10 @@ admin.initializeApp({
 const db = admin.firestore();
 const traders = db.collection('traders');
 const trades = db.collection('trades');
+
+app.use(express.json());
+
+app.post('/addPrediction', (req, res) => {
+  const result = await trades.add(req.body);
+  console.log('Added document with ID: ', result.id);
+}
