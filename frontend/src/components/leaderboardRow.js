1e1cfbd22d82afd62fb0b40b074a0033c8dfea8c
Allen Su <allensu2001@gmail.com>
2020-09-19 22:59:30 -0700

    made leaderboard work and not ugly

---
+++ frontend/src/components/leaderboardRow.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {BsArrowDownShort, BsArrowUpShort} from "react-icons/bs";
+
+export default class LeaderboardRow extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <tr>
+
+        <td className="text-center" style={{color: this.props.rank === 1 ? "gold" : (this.props.rank === 2 ? "silver" : (this.props.rank === 3 ? "#cd7f32 " : "white")) }}>
+          {this.props.rank}
+        </td>
+        <td class="text-center">{this.props.name}</td>
+        <td className="text-center">{this.props.points}</td>
+
+      </tr>
+    )
+  }
+}
