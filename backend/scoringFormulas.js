eab325e6b0ddf11aaaf1cd9b6f8e7c2aa2fbdd55
Armand Mousavi <amousavi@cs.washington.edu>
2020-09-19 23:09:24 -0700

    Update scoringFormulas.js
    corrections syntax

--- backend/scoringFormulas.js
+++ backend/scoringFormulas.js
@@ -1,11 +1,11 @@
 const { getCurrentPrice, getPriceTarget } = require('./stockData');
 const intervalScale =
 {
-  "hour" : 1,
-  "day" : 10,
-  "week" : 100,
-  "month" : 1000,
-  "year" : 10000
+  "hour" : 10,
+  "day" : 100,
+  "week" : 1000,
+  "month" : 10000,
+  "year" : 100000
 };
 async function getPredictionEnergyCost(prediction, stockSymbol) {
   let analyst = (await getPriceTarget(stockSymbol)).median;
@@ -19,14 +19,13 @@ async function predictionValue(prediction) {
   const currentPrice = await getCurrentPrice(prediction.stockSymbol);
   const predictedDifference = prediction.predictedAmount - prediction.currentAmount;
   const actualDifference = currentPrice - prediction.currentAmount;
-  const absActualDifference = Math.abs(actualDifference);
   const offScale = Math.abs(actualDifference - predictedDifference);
   // check if the predictedDifference and actualDifference
   // have opposite signs
-  if ( (predictedDifference >= 0) ^ (actualDifference < 0)) {
-    return 0;
+  if ((predictedDifference >= 0) ^ (actualDifference < 0)) {
+    const intervalScaleValue = interval[prediction.intervalType];
+    const result = intervalScaleValue / offScale;
+    return Math.ceil(result);
   }
-  const intervalScaleValue = interval[prediction.intervalType];
-  const result = intervalScaleValue * absActualDifference / offScale
-  return Math.ceil(result);
+  return 0;
 }
