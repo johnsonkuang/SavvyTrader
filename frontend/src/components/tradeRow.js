b2075a4fbba497a88d854dfe7505dd2668904dd4
Johnson Kuang <johnsonkuang@outlook.com>
2020-09-19 20:57:41 -0700

    connected trades table to api

---
+++ frontend/src/components/tradeRow.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { BsArrowUpShort, BsArrowDownShort } from 'react-icons/bs';
+
+export default class TradeRow extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            proximity: true
+        };
+    }
+
+    componentDidMount(){
+        this.setState({
+            proximity: Math.abs(this.props.predPrice - this.props.currPrice)/this.props.currPrice <= 0.05
+        })    
+    }
+    
+    render() {
+        return (
+            <tr>
+                <td class="text-center">{this.props.symbol}</td>
+                <td class="text-center">{this.props.currPrice}</td>
+                <td class="text-center" style={{color: this.state.proximity ? "#28a745" : "#dc3545"}}>
+                    {this.props.predPrice} {this.state.proximity ? <BsArrowUpShort /> : <BsArrowDownShort />}
+                </td>
+                <td>{this.props.timePred.replace("(Pacific Daylight Time)", "")}</td>
+                <td class="text-center">{this.props.type}</td>
+                <td >{this.props.endTime.replace("(Pacific Daylight Time)", "")}</td>
+            </tr>
+        )
+    }
+}
--- frontend/src/components/trades.js
+++ frontend/src/components/trades.js
@@ -1,12 +1,92 @@
 import React, {Component } from 'react';
+import TradeRow from './tradeRow';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table'
+import axios from 'axios';
+import dayjs from 'dayjs';
 
+const USE_MOCK_DATA = false;
 
+const SERVER_API_URL = 'http://localhost:8080/getPredictions';
+
+const mockData = [{"currentAmount": 100, "dateMade": "2020-08-19 17:00:0", "intervalType": "hour", "predictedAmount": 200, "stockSymbol": "ARMA", "trader": "vishal"}];
 
 class Trades extends Component {
+
+  tradeList = () => {                  
+    console.log(this.state.trades);
+    const tradeList = this.state.trades.map((trade, index) => 
+      <TradeRow 
+        key={index}
+        symbol={trade["stockSymbol"]}
+        currPrice={trade["currentAmount"]}
+        predPrice={trade["predictedAmount"]}
+        timePred={trade["dateMade"].toString()}
+        type={trade["intervalType"]}
+        endTime={trade["endDate"].toString()}
+      />                    
+    );
+
+    return(
+      <tbody>
+        {tradeList}
+      </tbody>
+    )
+  };
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      trades : []
+    }
+  }
+
+  componentDidMount(){
+    this.getPredictions("vishal").then((data) => 
+      {
+      this.setState({
+        trades : data
+      })}
+    );
+  }
+
+  async getPredictions(user){
+    if(USE_MOCK_DATA) {
+      return mockData;
+    }
+    
+    try {
+			const response = await axios.get(SERVER_API_URL, {
+        params : {
+          user
+        }
+      });
+			const data = response.data;
+			data.forEach((c) =>{
+         c.dateMade = new Date(c.dateMade._seconds * 1000);
+         c.endDate = new Date(c.dateMade);
+         if(c.intervalType === "hour") {
+           if(c.endDate.getHours() === 12){
+             c.endDate.setDate(c.endDate.getDate() + 1);
+           }
+           c.endDate.setHours(c.endDate.getHours() + 1);
+         } else if (c.intervalType === "day"){ //can change to set type
+          c.endDate.setDate(c.endDate.getDate() + 1);
+         } else {
+            c.endDate.setDate(c.endDate.getDate() + 7); //try not to make week predictions
+         }
+        });
+			console.log(data);
+			return data;
+		} catch (error) {
+			console.error(error);
+			return [];
+		}
+  }
+
   render(){
     return(
       <Container>
@@ -25,16 +105,7 @@ class Trades extends Component {
                   <th>Prediction End Time</th>
                 </tr>
                 </thead>
-                <tbody>
-                <tr>
-                  <td>AAPL</td>
-                  <td>$1</td>
-                  <td>$2</td>
-                  <td>IDK</td>
-                  <td>Hour</td>
-                  <td>IDK</td>
-                </tr>
-                </tbody>
+                {this.tradeList()}
               </Table>
             </div>
           </Col>
